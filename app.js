d21a652ea55576a8350c1fe40eb4903c14ad24bc
mgerety <michaeldgerety@gmail.com>
2020-12-20 17:21:47 -0800

    updates

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const input = document.getElementById('input');
 const todoList = document.getElementById('todo-list');
 const list = document.getElementById('list');
 const filterControl = document.getElementById('filter-control');
+const clearButton  = document.getElementById('clear-completed');
+let LIST = [];
+let id = 0;
+const COMPLETE = 'fa-check-circle';
+const INCOMPLETE = 'fa-circle';
+const LINE_THROUGH = 'line-through';
+let checkButton = document.getElementsByTagName("i");
 
 
 // THEME TOGGLE
@@ -14,3 +21,61 @@ theme.addEventListener('click', () => {
     filterControl.classList.toggle('dark');
     list.classList.toggle('dark');
 });
+
+// INPUT EVENT LISTENER
+document.addEventListener('keyup', function(event) {
+    if(event.keyCode == 13) {
+        const toDo = input.value;
+        if(toDo) {
+            addToDo(toDo, id, false);
+            //AMEND LIST
+            LIST.push( {
+                name: toDo,
+                id: id,
+                done: false,
+            });
+        }
+        //RESET INPUT VALUE TO BLANK
+        input.value = "";
+        id++;
+    }   
+});
+
+// LIST ITEM TOGGLE EVENT LISTENER
+list.addEventListener('click', function(event){
+    let element = event.target;
+    completeToDo(element);
+    toggleDone(element);
+})
+
+// CLEAR COMPLETED
+
+
+// ADD TODO
+function addToDo(toDo, id, done) {
+    const DONE = done ? COMPLETE : INCOMPLETE;
+    const LINE = done ? LINE_THROUGH : "";
+    const text = `<li class="item">
+                    <i class="far ${DONE}" id="${id}"></i>
+                    <p class="text ${LINE}">${toDo}</p>
+                </li>`
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, text);
+}
+
+// COMPLETE TODO
+function completeToDo (element) {
+    element.classList.toggle(COMPLETE);
+    element.classList.toggle(INCOMPLETE);
+    element.parentNode.querySelector('.text').classList.toggle
+    (LINE_THROUGH);
+}
+
+function toggleDone (element) {
+    
+}
+
+
+
+
+
